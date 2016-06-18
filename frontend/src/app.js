3e18264645e63e690c260a561a0cbb839ccd07ea
leviathan88 <levigithuber@gmail.com>
2016-06-18 19:30:29 +0200

    what

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -37,73 +37,7 @@ import {APP_BASE_HREF} from 'angular2/router'
         </li>
       </ul>
     </nav>
-    <div class="hidden-md-up">
-      <h2 class="text-xs-center">Exploring MEAN</h2>
-    </div>
-    <div class="hero hidden-sm-down">
-      <h1 class="display-3">Exploring MEAN</h1>
-      <p class="lead">Becoming fullstack expert :)</p>
-    </div>
-    <div class="card-deck home-sections ">
-      <div class="card yoda">
-        <div class="card-img-top"></div>
-        <div class="card-block">
-          <h4 class="card-title">Jedi Masters</h4>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-      <div class="card weapons">
-        <div class="card-img-top"></div>
-        <div class="card-block">
-          <h4 class="card-title">Weapons</h4>
-          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-      <div class="card ships">
-        <div class="card-img-top"></div>
-        <div class="card-block">
-          <h4 class="card-title">Ships</h4>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-    <section class="new-jedi">
-      <header>
-        <h5><span class="highlight">New</span> Jedi</h5>
-      </header>
-      <div class="list-group list-group-lg">
-        <div class="list-group-item">
-         <p>Yoda</p>
-        </div>
-        <div class="list-group-item">
-         <p>Obi Wan</p>
-        </div>
-        <div class="list-group-item">
-         <p>Luke Skywalker</p>
-        </div>
-      </div>
-    </section>
-    <section class="new-logins">
-      <header>
-        <h5><span class="highlight">Lastest</span> Logins</h5>
-      </header>
-      <div class="list-group list-group-lg">
-        <div class="list-group-item">
-         <p>Darth Maul</p>
-        </div>
-        <div class="list-group-item">
-         <p>Darth Vader</p>
-        </div>
-        <div class="list-group-item">
-         <p>Senator Palpatine</p>
-        </div>
-      </div>
-    </section>
-    </div>
+    <router-outlet></router-outlet>
   </div>
   `
 })
