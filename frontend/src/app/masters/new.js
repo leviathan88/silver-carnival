0076a750448cf00bed78ba8553db2385506c46f1
leviathan88 <levigithuber@gmail.com>
2016-06-20 18:24:59 +0200

    creating list and new in masters

---
+++ frontend/src/app/masters/new.js
@@ -0,0 +1,11 @@
+import { Component, View } from 'angular/core';
+
+@Component({
+  selector: 'master-new'
+})
+
+@View({
+  template: `<h1>New Jedi Master</h1>`
+})
+
+export default class MasterNew{};
