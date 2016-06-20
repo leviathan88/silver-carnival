458fac3c5c49f015270fb788d2ca5ec185aa2104
leviathan88 <levigithuber@gmail.com>
2016-06-20 19:12:05 +0200

    including link for new master and listing masters

--- frontend/src/app/masters/list.js
+++ frontend/src/app/masters/list.js
@@ -1,11 +1,18 @@
-import { Component, View } from 'angular2/core';
+import { Component, View } from 'angular2/core'
+import { ROUTER_DIRECTIVES } from 'angular2/router'
 
 @Component({
   selector: 'master-list'
 })
 
 @View({
-  template: `<h1>Jedi Masters</h1>`
+  directives: [ ROUTER_DIRECTIVES ],
+  template: `
+    <h1>Jedi Masters<a class='btn btn priamry' [routerLink]="['/MasterNew']">New</a></h1>
+    <ul>
+      <li> <a [routerLink]="['/MasterView', {id: 12345}]">Yoda</a> </li>
+    </ul>
+  `
 })
 
-export default class MasterList{};
+export default class MasterList{}
